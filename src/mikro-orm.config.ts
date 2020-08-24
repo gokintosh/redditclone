e0e1a688b7da54f0530e64b90c28adff430ea6e0
gokintosh <gokul.kannambally_balachandran@student.po.edu.pl>
2020-08-24 14:52:13 +0200

    add server scripts

---
+++ src/mikro-orm.config.ts
@@ -0,0 +1,19 @@
+import { Post } from "./entities/Posts";
+import { __prod__ } from "./constants";
+import {MikroORM} from "@mikro-orm/core"
+import path from 'path'
+
+export default{
+    migrations: {
+    
+        path: path.join(__dirname,'./migrations'), // path to the folder with migrations
+        pattern: /^[\w-]+\d+\.[tj]s$/, // regex pattern for the migration files
+    
+  },
+       entities:[Post],
+       dbName:'lireddit',
+       user:'gokul',
+       password:'gokul',
+       type:'postgresql',
+       debug:!__prod__,
+}as Parameters<typeof MikroORM.init>[0];
